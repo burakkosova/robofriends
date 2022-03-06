3516a9852d893d8b0f9b270eab7e9e0cea50c03b
Burak Kosova <burak.kosova@bahcesehir.edu.tr>
2022-03-07 02:28:31 +0300

    state management with redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,29 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
+import { setSearchField, requestRobots } from "../actions";
+import { connect } from "react-redux";
 
-function App() {
-  const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchField] = useState("");
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    isPending: state.requestRobots.isPending,
+    robots: state.requestRobots.robots,
+    error: state.requestRobots.error,
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  onSearchChange: event => dispatch(setSearchField(event.target.value)),
+  onRequestRobots: () => dispatch(requestRobots()),
+});
+
+function App(props) {
+  const { onSearchChange, searchField, onRequestRobots, robots, isPending } =
+    props;
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => setRobots(users));
+    onRequestRobots();
   }, []);
 
-  const onSearchChange = (event) => {
-    setSearchField(event.target.value);
-  };
-
-  const filteredRobots = robots.filter((robot) =>
-    robot.name.toLowerCase().includes(searchfield.toLowerCase())
+  const filteredRobots = robots.filter(robot =>
+    robot.name.toLowerCase().includes(searchField.toLowerCase())
   );
 
-  return !robots.length ? (
+  return isPending ? (
     <h1>Loading...</h1>
   ) : (
     <div className="tc">
@@ -38,4 +48,4 @@ function App() {
   );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
-import App from './containers/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { applyMiddleware, createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import App from "./containers/App";
+import { searchRobots, requestRobots } from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
