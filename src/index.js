27e3250b008b7046fd6dbea93ae5a3584138993f
Burak Kosova <burak.kosova25@gmail.com>
2022-03-12 07:55:31 +0300

    enabling service worker for pwa features

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorkerRegistration";
 import "tachyons";
 import App from "./containers/App";
 import { searchRobots, requestRobots } from "./reducers";
@@ -27,4 +27,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
